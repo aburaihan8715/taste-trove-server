51928428ba36395a33a26938d7eaeb966963912a
aburahan8721 <aburaihan8721@gmail.com>
2023-05-25 20:42:14 +0600

    environment variable setup done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const data = require("./data/data.json");
 
-app.get('/', (req, res) => {
-  res.send('Hello World update!')
-})
+// middleware
+app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+// home route
+app.get("/", (req, res) => {
+  res.send("Hello World From Home Route!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(data);
+});
