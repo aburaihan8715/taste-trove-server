32f6a642e29da2f9f6376d8cd95d6d3bf37d7d96
aburahan8721 <aburaihan8721@gmail.com>
2023-05-27 18:24:55 +0600

    server requirment done

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const data = require("./data/data.json");
+const chefsData = require("./data/chefData.json");
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
@@ -17,7 +18,9 @@ app.get("/", (req, res) => {
   res.send("Hello World From Home Route!");
 });
 
-// users route
-app.get("/users", (req, res) => {
-  res.send(data);
+// chefs routes
+app.get("/chefs", (req, res) => {
+  res.send(chefsData);
 });
+
+
